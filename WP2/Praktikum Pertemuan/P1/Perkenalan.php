86b3ad88c7601e2d972009d409e4eb666ab43eb5
Hafizh Umar Syafiqh <hafizhsyafiqh@gmail.com>
2020-10-26 20:13:34 +0700

    first commit

---
+++ WP2/Praktikum Pertemuan/P1/Perkenalan.php
@@ -0,0 +1,11 @@
+<?php
+class Perkenalan extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalan</h1>";
+        echo "Nama Saya Hafizh Umar Syafiqh <br>
+            Saya Tinggal di Adiwerna<br>
+            Umurku 20 tahun";
+    }
+}
